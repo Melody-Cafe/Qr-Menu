68f767d5bfc266a560ef4862033d74c73b798276
Mahammad <129037468+Mahammad-Aslanzade@users.noreply.github.com>
2024-10-10 06:15:49 -0700

    Update isti_yemekler.js

--- data/menuItems/isti_yemekler.js
+++ data/menuItems/isti_yemekler.js
@@ -1,86 +1,128 @@
 export const isti_yemekler = [
+    
     {
         id: 1,
         title: "Tuşonka sadə",
         gram: "",
         price: 9.99
     },
+    
     {
         id: 2,
         title: "Can əti tərəvəzlə",
         gram: "",
         price: 29.99
     },
+    
     {
         id: 3,
         title: "Çoban qovurması sacda",
         gram: "",
         price: 11.99
     },
+    
     {
         id: 4,
         title: "Dana əti sacda",
         gram: "",
         price: 24.99
     },
+    
     {
         id: 5,
         title: "Qarışıq sac",
         gram: "",
         price: 29.99
     },
+    
     {
         id: 6,
         title: "Quzu sacı",
         gram: "",
         price: 27.99
     },
+    
     {
         id: 7,
         title: "Sac broyler",
         gram: "",
         price: 23.99
     },
+    
     {
         id: 8,
         title: "Sac kənd çolpası",
         gram: "",
         price: 21.99
     },
+    
     {
         id: 9,
         title: "Nar Qovurma can əti",
         gram: "",
         price: 17.99
     },
+    
     {
         id: 10,
         title: "Qaymaqlı ət",
         gram: "",
         price: ""
     },
+    
     {
         id: 11,
         title: "Qaymaqlı toyuq",
         gram: "",
         price: ""
     },
+    
     {
         id: 12,
         title: "Ətli Faxitos",
         gram: "",
         price: 16.99
     },
+    
     {
         id: 13,
         title: "Toyuq Faxitos",
         gram: "",
         price: 7.99
     },
+    
     {
         id: 14,
         title: "Tuşonka Kartof ilə",
         gram: "",
         price: 11.99
+    },
+    
+    {
+        id: 15,
+        title: "Ət Naços",
+        gram: "",
+        price: 10.99
+    },
+    
+    {
+        id: 16,
+        title: "Ət Langet",
+        gram: "Düyü / Kartof Free",
+        price: 17.99
+    },
+
+    {
+        id: 17,
+        title: "Çolpa Tabaka",
+        gram: "",
+        price: 14.99
+    },
+    
+    {
+        id: 18,
+        title: "Toyuq Broyler Tavada",
+        gram: "",
+        price: 12.99
     }
 ];
