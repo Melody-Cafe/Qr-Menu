fb2e2a853b8438d0a9f2487a012e4e71f8df538d
Mahammad <129037468+Mahammad-Aslanzade@users.noreply.github.com>
2024-10-10 06:27:29 -0700

    Add files via upload

---
+++ data/menuItems/kokteyl_shot.js
@@ -0,0 +1,64 @@
+export const kokteyl_shot=[
+
+    {
+        id: 1,
+        title: "Moxito Alkoqol Kokteyl",
+        gram: "",
+        price: 15
+    },
+    
+    {
+        id: 2,
+        title: "Moxito Sadə",
+        gram: "",
+        price: 9
+    },
+    
+    {
+        id: 3,
+        title: "Melody Pink",
+        gram: "",
+        price: 8
+    },
+
+    {
+        id: 4,
+        title: "Melody Special",
+        gram: "",
+        price: 10
+    },
+
+    {
+        id: 5,
+        title: "Olmega Shot",
+        gram: "",
+        price: 40
+    },
+
+    {
+        id: 6,
+        title: "Shot Set 6",
+        gram: "",
+        price: 17
+    },
+    
+    {
+        id: 7,
+        title: "Special Shot",
+        gram: "",
+        price: 23
+    },
+    {
+        id: 8,
+        title: "Tequilla Sunrise ",
+        gram: "",
+        price: 20
+    },
+    {
+        id: 9,
+        title: "W Shot",
+        gram: "",
+        price: 20
+    },
+
+]
